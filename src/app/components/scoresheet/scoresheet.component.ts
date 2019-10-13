e919f5be12bed747edc27649f83842d82a628af2
Coen Mooij <coenmooij@gmail.com>
2019-10-13 14:59:14 +0200

    Add colors and statustext for scoresheet

--- src/app/components/scoresheet/scoresheet.component.ts
+++ src/app/components/scoresheet/scoresheet.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Game } from '../../domain/game/game.interface';
+import { Round } from '../../domain/game/round.interface';
 
 @Component({
   selector: 'app-scoresheet',
@@ -12,6 +13,10 @@ export class ScoresheetComponent implements OnInit {
   teamOne = '';
   teamTwo = '';
 
+  TEGENPIT = 'TEGENPIT';
+  NAT = 'NAT';
+  PIT = 'PIT';
+
   ngOnInit(): void {
     this.setTeams(this.game.players);
   }
@@ -19,5 +24,22 @@ export class ScoresheetComponent implements OnInit {
   setTeams(players: string[]): void {
     this.teamOne = `${players[0]} & ${players[2]}`;
     this.teamTwo = `${players[1]} & ${players[3]}`;
+  }
+
+  getTeamScore(team: number, roundIndex: number, round: Round): string | number {
+    if (this.getPlayingTeamForRound(roundIndex) === team) {
+      return round.tegenpit ? this.TEGENPIT : (round.nat ? this.NAT : this.getPoints(team, round));
+    } else {
+      return round.pit ? this.PIT : this.getPoints(team, round);
+    }
+  }
+
+  getPlayingTeamForRound(round: number): number {
+    const teamIndex = round % 2;
+    return teamIndex + 1;
+  }
+
+  getPoints(team: number, round: Round): number {
+    return team === 1 ? round.teamOnePoints : round.teamTwoPoints;
   }
 }
